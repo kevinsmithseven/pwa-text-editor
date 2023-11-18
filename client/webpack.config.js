cd3a6736cc9c3d44daf82a7b349f9fc68f4ac697
kevinsmithseven <kevinsmithseven@gmail.com>
2023-11-18 12:08:49 -0600

    added webpackpwamanifest plugin to webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,25 @@ module.exports = () => {
       }),
       new GenerateSW({
         swDest: '/src-sw.js'
-      })
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'just another text editor',
+        short_name: 'J.A.T.E.',
+        description: 'just another text editor',
+        background_color: '#31a9e1',
+        theme_color: '#31a9e1',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
